0a702a9503ae40f279e205ce06a3b314d0c2bb69
Justin Gardner <justin@rhynorater.com>
2024-07-12 06:03:18 -0500

    1.14.3

--- extendify/public/build/extendify-help-center-99f6e8bb9d64cd0c40ef.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => 'a5b719fa475054e7a94b');
